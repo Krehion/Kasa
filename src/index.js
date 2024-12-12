dd9b3cb8174a26d11f4deb56f0880c6abb8cc0b3
Krehion <krehion@hotmail.com>
2024-12-12 10:59:24 +0100

    :construction_worker: [Build] add 404 page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import "./style/style.scss";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
+import NotFound from "./pages/notFound/NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,6 +13,7 @@ root.render(
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
---
+++ src/pages/notFound/NotFound.js
@@ -0,0 +1,19 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+	return (
+		<div className="error">
+			<Header />
+			<main>
+				<p className="error__number">404</p>
+				<p className="error__description">Oups! La page que vous demandez n'existe pas.</p>
+				<Link to="/" className="error__link">
+					Retourner sur la page d'accueil
+				</Link>
+			</main>
+			<Footer />
+		</div>
+	);
+}
