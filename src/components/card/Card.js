36ef041581e2c784443eefea2f0dde556bad9cda
Krehion <krehion@hotmail.com>
2024-12-17 14:04:06 +0100

    :recycle: [Refactor] change <a> tag to <Link> component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,12 @@
 import "../../style/components/_card.scss";
+import { Link } from "react-router-dom";
 
 function Card({ id, title, cover }) {
 	return (
 		<article className="card">
-			<a href={`/accomodation/${id}`} className="card__link" title={title}>
+			<Link to={`/accomodation/${id}`} className="card__link" title={title}>
 				{""}
-			</a>
+			</Link>
 			<img src={cover} alt={title} className="card__bkgd" />
 			<div className="card__overlay"></div>
 			<h3 className="card__title">{title}</h3>
