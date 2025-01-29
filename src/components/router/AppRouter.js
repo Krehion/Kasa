4bd4d426ec75efc638437a3431bb35e69787ecff
Krehion <krehion@hotmail.com>
2025-01-29 14:20:14 +0100

    :recycle: adjust routes to handle local or gh-pages environments

--- src/components/router/AppRouter.js
+++ src/components/router/AppRouter.js
@@ -6,14 +6,14 @@ import Accomodation from "../../pages/accomodation/Accomodation";
 import NotFound from "../../pages/notFound/NotFound";
 
 const AppRouter = () => {
-	const basename = process.env.NODE_ENV === "production" ? "/Kasa" : "/"; // handle local or github-pages environment dynamically. "Kasa" = repository name
+	const basename = process.env.NODE_ENV === "production" ? "/Kasa" : "/"; // handle local or GitHub Pages environment
 
 	return (
 		<BrowserRouter basename={basename}>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/accomodation/:id" element={<Accomodation />} />
-				<Route path="/about" element={<About />} />
+				<Route path="" element={<Home />} />
+				<Route path="accomodation/:id" element={<Accomodation />} />
+				<Route path="about" element={<About />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
